1d651fb3a5a259a9a18d94645e03db47fcb4c627
lubit <xiao.pl@qq.com>
2018-09-04 14:55:00 +0800

    add support [kafka | redis | mysql |clickhouse]

--- klient.go
+++ klient.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"crypto/tls"
-	"fmt"
 	"log"
 	"os"
 	"sort"
-	"strings"
 
-	"github.com/Shopify/sarama"
 	"github.com/urfave/cli"
 )
 
@@ -21,6 +17,7 @@ type Kflags struct {
 	Shell   string
 	Brokers string
 	Topic   string
+	Group   string
 }
 
 var kflags Kflags
@@ -60,6 +57,10 @@ func main() {
 			Destination: &kflags.Topic,
 		},
 		cli.StringFlag{
+			Name:        "group, G",
+			Destination: &kflags.Group,
+		},
+		cli.StringFlag{
 			Name:        "shell, e",
 			Destination: &kflags.Shell,
 		},
@@ -67,27 +68,27 @@ func main() {
 	app.Flags = flags
 
 	// Default Command: Kafka
-	app.Action = kafkaShell
+	app.Action = KafkaShell
 	app.Commands = []cli.Command{
 		{
 			Name:   "kafka",
 			Flags:  app.Flags,
-			Action: kafkaShell,
+			Action: KafkaShell,
 		},
 		{
 			Name:   "redis",
 			Flags:  flags,
-			Action: redisShell,
+			Action: RedisShell,
 		},
 		{
 			Name:   "mysql",
 			Flags:  flags,
-			Action: mysqlShell,
+			Action: MysqlShell,
 		},
 		{
 			Name:   "clickhouse",
 			Flags:  flags,
-			Action: clickhouseShell,
+			Action: ClickhouseShell,
 		},
 	}
 
@@ -98,90 +99,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-type KafkaConfig struct {
-	Brokers  []string
-	Topic    string
-	User     string
-	Password string
-	consumer sarama.Consumer
-	client   sarama.Client
-}
-
-func kafkaShell(c *cli.Context) (err error) {
-
-	config := KafkaConfig{}
-	config.Brokers = strings.Split(kflags.Brokers, ",")
-	config.Topic = kflags.Topic
-	config.User = kflags.User
-	config.Password = kflags.Pswd
-
-	consumerConfig := sarama.NewConfig()
-	consumerConfig.Net.SASL.User = kflags.User
-	consumerConfig.Net.SASL.Password = kflags.Pswd
-	consumerConfig.Net.SASL.Handshake = true
-	consumerConfig.Net.SASL.Enable = true
-
-	consumerConfig.Net.TLS.Enable = true
-	tlsConfig := &tls.Config{
-		InsecureSkipVerify: true,
-		ClientAuth:         0,
-	}
-	consumerConfig.Net.TLS.Config = tlsConfig
-
-	fmt.Println("subcommands:", c.Args())
-	config.client, err = sarama.NewClient(config.Brokers, consumerConfig)
-	if err != nil {
-		fmt.Println("Unable to create kafka client " + err.Error())
-		return
-	}
-
-	config.consumer, err = sarama.NewConsumerFromClient(config.client)
-	if err != nil {
-		fmt.Println("Unable to create new kafka consumer", err, config.client)
-		return
-	}
-
-	partitions, err := config.client.Partitions(config.Topic)
-
-	if err != nil {
-		fmt.Println("Unable to fetch partition IDs for the topic", err, config.client, config.Topic)
-		return
-	}
-
-	fmt.Println("Partitions:", partitions)
-
-	topics, err := config.client.Topics()
-	if err != nil {
-		fmt.Println("Unable to fetch topics", err, config.client)
-		return
-	}
-	for _, v := range topics {
-		for _, p := range partitions {
-			offset, err := config.client.GetOffset(v, p, sarama.OffsetNewest)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Printf("Topic[%s] Partition[%.2d] Offset : %d \n", v, p, offset)
-		}
-	}
-
-	return
-
-}
-
-func redisShell(c *cli.Context) error {
-	fmt.Println(kflags)
-	return nil
-}
-
-func mysqlShell(c *cli.Context) error {
-	fmt.Println(kflags)
-	return nil
-}
-
-func clickhouseShell(c *cli.Context) error {
-	fmt.Println(kflags)
-	return nil
 }
